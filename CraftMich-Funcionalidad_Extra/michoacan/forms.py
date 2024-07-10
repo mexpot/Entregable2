5b21f548fcc4c5f4e7d6b263cdae7bff5ee38d64
unknown <utm210042ti@ut-morelia.edu.mx>
2024-07-09 18:59:41 -0600

    Apartado de Productos de Javier

---
+++ CraftMich-Funcionalidad_Extra/michoacan/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import CrearEvento,Eventos
+
+class CrearEventoForm(forms.ModelForm):
+    class Meta:
+        model = CrearEvento
+        fields = ['nombre','fecha','hora','lugar','tipo','descripcion']
+class EventosForm(forms.ModelForm):
+    class Meta:
+        model = Eventos
+        fields = ['imagen', 'evento', 'precio', 'descripcion']
+
